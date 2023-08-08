b1b8691a104f71368a5328fa03c707793aa41636
@SouravTiwari306 <souravany2000@gmail.com>
2023-08-08 22:31:05 +0530

    update

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -6,7 +6,7 @@ const SocialMediaIcons =()=>{
    href="https://www.linkedin.com/in/sourav-kumar-2a66611b8"
    target="_blank"
    rel="moreferrer">
-    <img alt="linkedin-link" src="/assets/linkedin.png"/>
+    <img alt="linkedin-link" src="assets/linkedin.png"/>
             </a>
 
 
@@ -14,20 +14,20 @@ const SocialMediaIcons =()=>{
    href="https://twitter.com/SouravTiwari749?t=Mtg2c1Pqq2FmBTvAcvRktQ&s=09"
    target="_blank"
    rel="moreferrer">
-    <img alt="twitter-link" src="/assets/twitter.png"/>
+    <img alt="twitter-link" src="assets/twitter.png"/>
             </a>
 
             <a className="hover:opacity-50 transition duration-500"
    href="https://www.facebook.com/profile.php?id=100007034865554"
    target="_blank"
    rel="moreferrer">
-    <img alt="facebook-link" src="/assets/facebook.png"/>
+    <img alt="facebook-link" src="assets/facebook.png"/>
             </a>
             <a className="hover:opacity-50 transition duration-500"
    href="https://instagram.com/sk_sourav_tiwari?igshid=MzNlNGNkZWQ4Mg=="
    target="_blank"
    rel="moreferrer">
-    <img alt="instagram-link" src="/assets/instagram.png"/>
+    <img alt="instagram-link" src="assets/instagram.png"/>
             </a>
         </div>
     )
